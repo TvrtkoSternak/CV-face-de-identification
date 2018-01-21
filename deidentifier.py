7889f627b2907ee99b944887cdb33a7fd7ae44e7
karmelastronaut <karmela.slacanac@fer.hr>
2018-01-21 17:59:42 +0100

    Add different gaussian filter function, add converting to png format

--- deidentifier.py
+++ deidentifier.py
@@ -1,24 +1,26 @@
-from scipy import fftpack
-import cv2, numpy
+from scipy import fftpack, stats
+import numpy
 import matplotlib.pyplot as plt
+from PIL import Image
 
 class Deidentifier:
     def __init__(self):
         pass
 
-    def gaussian_kernel(self, size, size_y=None):
-        size = int(size)
-        if not size_y:
-            size_y = size
-        else:
-            size_y = int(size_y)
-        x, y = numpy.mgrid[-size:size + 1, -size_y:size_y + 1]
-        g = numpy.exp(-(x ** 2 / float(size) + y ** 2 / float(size_y)))
-        return g / g.sum()
+    def gaussian_kernel(self, kernlen=43, nsig=3):
+        interval = (2 * nsig + 1.) / (kernlen)
+        x = numpy.linspace(-nsig - interval / 2., nsig + interval / 2., kernlen + 1)
+        kern1d = numpy.diff(stats.norm.cdf(x))
+        kernel_raw = numpy.sqrt(numpy.outer(kern1d, kern1d))
+        kernel = kernel_raw / kernel_raw.sum()
+        return kernel
     
     def deidentify(self, image_file, rectangles=None):
-        img = plt.imread(image_file)
-        gaussian = self.gaussian_kernel(10)
+        img2 = Image.open(image_file)
+        # converting to png
+        img2.save("Images/people-test.png")
+        img = plt.imread("Images/people-test.png")
+        gaussian = self.gaussian_kernel()
         gaussian_ft = fftpack.fft2(gaussian, shape=img.shape[:2], axes=(0, 1))
         img_ft = fftpack.fft2(img, axes=(0, 1))
         if rectangles:
@@ -35,8 +37,9 @@ class Deidentifier:
 
     def identify(self, img):
         image_blurred_ft = fftpack.fft2(img, axes=(0, 1))
-        gaussian = self.gaussian_kernel(10)
+        gaussian = self.gaussian_kernel()
         gaussian_ft = fftpack.fft2(gaussian, shape=img.shape[:2], axes=(0, 1))
+        # deconvolution by fourier transform
         image_deblurred_ft = image_blurred_ft / gaussian_ft[:, :, numpy.newaxis]
         image_deblurred = fftpack.ifft2(image_deblurred_ft, axes=(0, 1))
         image_deblurred = numpy.clip(image_deblurred, 0, 1)
@@ -44,7 +47,7 @@ class Deidentifier:
 
 if __name__ == '__main__':
     di = Deidentifier()
-    img = di.deidentify('Images/people-01.png')
+    img = di.deidentify('Images/people-01.jpg')
     plt.figure()
     plt.imshow(img.real)
     deblurred = di.identify(img)
